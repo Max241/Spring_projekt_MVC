25753889426f248b285d72ab96afd3960b1b7713
Bartek_Rozga <bartek130@gmail.com>
2021-01-17 12:07:02 +0100

    J-Unit - Default test

---
+++ src/test/java/Default_Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Default_Test {
+
+    @Test
+    void simpleTest() {
+        assertEquals(true, false);
+    }
+
+}
