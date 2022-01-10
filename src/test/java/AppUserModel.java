a9b1b5960f6f2d2204867abe3ffe42dec193b09f
Bartek_Rozga <bartek130@gmail.com>
2022-01-10 21:58:07 +0100

    AppUserModelTest, checking Jenkins

---
+++ src/test/java/AppUserModel.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import pl.dmcs.brozga.model.AppUser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppUserModel {
+
+    @Test
+    void appUserEqualsToString() {
+        AppUser appUser = new AppUser();
+        appUser.setName("Doctor");
+        appUser.setSurname("Good");
+        appUser.setEmail("doctor@op.pl");
+
+        assertEquals("Doctor Good doctor@op.pl", appUser.toString());
+    }
+}
+
